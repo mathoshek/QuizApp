1f0f2b0ef5bbc5e1998704969955fc06b8956232
Albert Chmilevski <albert.chmilevski@gmail.com>
2015-05-25 21:44:57 +0300

    added repository module

--- QuizApp/Controllers/AdminController.cs
+++ QuizApp/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace QuizApp.Controllers
 {
     public class AdminController : Controller
     {
+        private QuizAppRepo repo = new QuizAppRepo();
 
         public ActionResult Index()
         {
+            ViewBag.Users = repo.getUsers();
             return View();
         }
 
