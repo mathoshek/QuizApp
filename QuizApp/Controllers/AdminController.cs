66e5cf47ae5e60d09b5df6d57b46940e4e143c0d
danutHurjui <danut.hurjui@info.uaic.ro>
2015-05-26 22:20:01 +0300

    admin front end questions part

--- QuizApp/Controllers/AdminController.cs
+++ QuizApp/Controllers/AdminController.cs
@@ -14,5 +14,12 @@ namespace QuizApp.Controllers
             return View();
         }
 
+        public ActionResult Question()
+        {         
+            return View();
+        }
+
+
+
     }
 }
