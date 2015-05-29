c1f67e3a6b04064d48bc409153e37b5812559bbb
Albert Chmilevski <albert.chmilevski@gmail.com>
2015-05-29 14:15:58 +0300

    StartQuiz

---
+++ QuizApp/Models/StartQuizModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizApp.Models
+{
+    public class StartQuizModel
+    {
+        public string QuizInstanceId { get; set; }
+        public string Step { get; set; }
+    }
+}
---
+++ QuizApp/Models/SustainQuizModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizApp.Models
+{
+    public class SustainQuizModel
+    {
+        public string QuizInstanceId { get; set; }
+        public string Step { get; set; }
+    }
+}
