6c976ade8f0e9c884529b557a869c320dfcec334
danutHurjui <danut.hurjui@info.uaic.ro>
2015-05-29 13:18:21 +0300

    plus

---
+++ QuizApp/Models/SustainQuiz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizApp.Models
+{
+    public class SustainQuiz
+    {
+        public int QuizId { get; set; }
+
+    }
+}
